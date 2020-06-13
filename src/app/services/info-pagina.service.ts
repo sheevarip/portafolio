cc8ced2ae5106556996020a474d56dcf94e60e54
sheevarip <tomatin77@gmail.com>
2020-06-12 23:11:45 -0500

    firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,16 +8,31 @@ import { InfoPagina } from '../interfaces/info-pagina';
 export class InfoPaginaService {
   info: InfoPagina ={};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
 
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+
+
+   private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
     .subscribe( resp =>{
 
           this.cargada = true;
           this.info = resp;
+          
+        });
+   }
+   private cargarEquipo(){
+    this.http.get('https://angular-html-d58bf.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) =>{
+      this.equipo = resp;
           console.log(resp);
         });
-
    }
 }
+
